aa691c8f882936b3284d9795f160c32ea985eb86
sean-mcelwain <79949547+sean-mcelwain@users.noreply.github.com>
2021-08-17 20:49:08 -0500

    update

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # TripBook
 
-<<<<<<< HEAD
-The place for trips!!!
-=======
 Let's travel
->>>>>>> 704d7f5ea7fd1e38ea163f6e476030fa128934b7
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
+import {BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 
 import TripList from '../components/TripList';
 import TripForm from '../components/TripForm';
+import logo from '../logo.png';
+import '../App.css';
 
 import { QUERY_TRIPS } from '../utils/queries';
 
@@ -11,27 +14,68 @@ const Home = () => {
   const trips = data?.trips || [];
 
   return (
-    <main>
-      <div className="flex-row justify-center">
-        <div
-          className="col-12 col-md-10 mb-3 p-3"
-          style={{ border: '1px dotted #1a1a1a' }}
-        >
-          <TripForm />
+    <div className="container-fluid">
+
+
+        <div className="row bg-light whiteCon">
+            <div className="col-12">
+                <div className="container">
+                    <div className="row btnRow">
+                        {/* Home page main buttons */}
+                        <div className="col-6 text-center p-5">
+                            <button className="btn btn-primary p-4">View Your Trips</button>
+                        </div>
+                        <div className="col-6 p-5 text-center">
+                          <button className="btn btn-primary p-4">Take a Trip!</button>
+                        </div>
+                        
+                          <div className="flex-row justify-center">
+                              <div
+                              className="col-12 col-md-10 mb-3 p-3"
+                              style={{ border: '1px dotted #1a1a1a' }}
+                              >
+                              <TripForm />
+                              </div>
+                              <div className="col-12 col-md-8 mb-3">
+                              {loading ? (
+                                  <div>Loading...</div>
+                              ) : (
+                                  <TripList
+                                  trips={trips}
+                                  title="Let's Go Bro!"
+                                  />
+                              )}
+                              </div>
+                          </div>
+
+                        {/* Trip images */}
+                        <div className="col-12 text-center">
+                              <img className="travelImg" src="/Images/1.jpg"></img>
+                              <img className="travelImg" src="/Images/2.jpg"></img>
+                              <img className="travelImg" src="/Images/3.jpg"></img>
+                              <img className="travelImg" src="/Images/4.jpg"></img>
+                              <img className="travelImg" src="/Images/5.jpg"></img>
+                              <img className="travelImg" src="/Images/6.jpg"></img>
+                              <img className="travelImg" src="/Images/7.jpg"></img>
+                              <img className="travelImg" src="/Images/8.jpg"></img>
+                              <img className="travelImg" src="/Images/9.jpg"></img>
+                              <img className="travelImg" src="/Images/10.jpg"></img>
+                              <img className="travelImg" src="/Images/12.jpg"></img>
+                              <img className="travelImg" src="/Images/13.jpg"></img>
+                              <img className="travelImg" src="/Images/14.jpg"></img>
+                              <img className="travelImg" src="/Images/15.jpg"></img>
+                              <img className="travelImg" src="/Images/16.jpg"></img>
+                              <img className="travelImg" src="/Images/17.jpg"></img>
+                              <img className="travelImg" src="/Images/18.jpg"></img>
+                              <img className="travelImg" src="/Images/19.jpg"></img>
+                              <img className="travelImg" src="/Images/20.jpg"></img>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="col-12 col-md-8 mb-3">
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <TripList
-              trips={trips}
-              title="Let's Go Bro!"
-            />
-          )}
-        </div>
-      </div>
-    </main>
-  );
-};
+    </div>
+);
+}
 
 export default Home;
