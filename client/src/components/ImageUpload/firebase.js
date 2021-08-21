48107256b8c61acac43a667926425e6dca2615c9
Tom Hanks <amaresch@gmail.com>
2021-08-21 13:04:35 -0500

    updated firebase config

--- client/src/components/ImageUpload/firebase.js
+++ client/src/components/ImageUpload/firebase.js
@@ -1,12 +1,13 @@
 import firebase from "firebase";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDDBveFJ1Y4j5alu7ZssxbLtAO6WZlH1SU",
-  authDomain: "tripbook-d2d2f.firebaseapp.com",
-  projectId: "tripbook-d2d2f",
-  storageBucket: "tripbook-d2d2f.appspot.com",
-  messagingSenderId: "805907316680",
-  appId: "1:805907316680:web:3177aac0c4f6022f3e0afd",
-  measurementId: "G-PTZ80R8RMF",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 firebase.initializeApp(firebaseConfig);
 var storage = firebase.storage();
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
-import {BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 
-import TripList from '../components/TripList';
-import TripForm from '../components/TripForm';
-import logo from '../logo.png';
-import '../index.css';
+import TripList from "../components/TripList";
+import TripForm from "../components/TripForm";
+import logo from "../logo.png";
+import "../index.css";
+import AllImages from "../components/ImageUpload/allImages";
 
-import { QUERY_TRIPS } from '../utils/queries';
+import { QUERY_TRIPS } from "../utils/queries";
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_TRIPS);
@@ -15,62 +16,61 @@ const Home = () => {
 
   return (
     <div className="row bg-light whiteCon pb-5">
-        <div className="col-12">
-            <div className="row btnRow">
-                {/* Home page main buttons */}
-                <div className="col-6 text-center p-5">
-                    <Link className="btn btn-primary" to="/trips">View Your Trips</Link>
-                </div>
-                <div className="col-6 p-5 text-center">
-                    <button className="btn btn-primary">Take a Trip!</button>
-                </div>
-                <div className="col-12">
-                    <div className="flex-row justify-center text-center">
-                        <div
-                        className="col-12 col-md-10 mb-3 p-3"
-                        style={{ border: '1px dotted #1a1a1a' }}
-                        >
-                        <TripForm />
-                        </div>
-                        <div className="col-12 col-md-8 mb-3">
-                        {loading ? (
-                            <div>Loading...</div>
-                        ) : (
-                            <TripList
-                            trips={trips}
-                            title="Let's Go Bro!"
-                            />
-                        )}
-                        </div>
-                    </div>
-                </div>
-
-                {/* Trip images */}
-                <div className="col-12 text-center">
-                        <img className="travelImg" src="/Images/1.jpg"></img>
-                        <img className="travelImg" src="/Images/2.jpg"></img>
-                        <img className="travelImg" src="/Images/3.jpg"></img>
-                        <img className="travelImg" src="/Images/4.jpg"></img>
-                        <img className="travelImg" src="/Images/5.jpg"></img>
-                        <img className="travelImg" src="/Images/6.jpg"></img>
-                        <img className="travelImg" src="/Images/7.jpg"></img>
-                        <img className="travelImg" src="/Images/8.jpg"></img>
-                        <img className="travelImg" src="/Images/9.jpg"></img>
-                        <img className="travelImg" src="/Images/10.jpg"></img>
-                        <img className="travelImg" src="/Images/12.jpg"></img>
-                        <img className="travelImg" src="/Images/13.jpg"></img>
-                        <img className="travelImg" src="/Images/14.jpg"></img>
-                        <img className="travelImg" src="/Images/15.jpg"></img>
-                        <img className="travelImg" src="/Images/16.jpg"></img>
-                        <img className="travelImg" src="/Images/17.jpg"></img>
-                        <img className="travelImg" src="/Images/18.jpg"></img>
-                        <img className="travelImg" src="/Images/19.jpg"></img>
-                        <img className="travelImg" src="/Images/20.jpg"></img>
-                </div>
+      <div className="col-12">
+        <div className="row btnRow">
+          {/* Home page main buttons */}
+          <div className="col-6 text-center p-5">
+            <Link className="btn btn-primary" to="/trips">
+              View Your Trips
+            </Link>
+          </div>
+          <div className="col-6 p-5 text-center">
+            <button className="btn btn-primary">Take a Trip!</button>
+          </div>
+          <div className="col-12">
+            <div className="flex-row justify-center text-center">
+              <div
+                className="col-12 col-md-10 mb-3 p-3"
+                style={{ border: "1px dotted #1a1a1a" }}
+              >
+                <TripForm />
+              </div>
+              <div className="col-12 col-md-8 mb-3">
+                {loading ? (
+                  <div>Loading...</div>
+                ) : (
+                  <TripList trips={trips} title="Let's Go Bro!" />
+                )}
+              </div>
             </div>
+          </div>
+
+          {/* Trip images */}
+          <div className="col-12 text-center">
+            <img className="travelImg" src="/Images/1.jpg"></img>
+            <img className="travelImg" src="/Images/2.jpg"></img>
+            <img className="travelImg" src="/Images/3.jpg"></img>
+            <img className="travelImg" src="/Images/4.jpg"></img>
+            <img className="travelImg" src="/Images/5.jpg"></img>
+            <img className="travelImg" src="/Images/6.jpg"></img>
+            <img className="travelImg" src="/Images/7.jpg"></img>
+            <img className="travelImg" src="/Images/8.jpg"></img>
+            <img className="travelImg" src="/Images/9.jpg"></img>
+            <img className="travelImg" src="/Images/10.jpg"></img>
+            <img className="travelImg" src="/Images/12.jpg"></img>
+            <img className="travelImg" src="/Images/13.jpg"></img>
+            <img className="travelImg" src="/Images/14.jpg"></img>
+            <img className="travelImg" src="/Images/15.jpg"></img>
+            <img className="travelImg" src="/Images/16.jpg"></img>
+            <img className="travelImg" src="/Images/17.jpg"></img>
+            <img className="travelImg" src="/Images/18.jpg"></img>
+            <img className="travelImg" src="/Images/19.jpg"></img>
+            <img className="travelImg" src="/Images/20.jpg"></img>
+          </div>
         </div>
+      </div>
     </div>
-);
-}
+  );
+};
 
 export default Home;
