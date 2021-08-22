51f0664e5de7d78f0742a744369f9d1e8617f8d3
Grant Shell <grantshell33@icloud.com>
2021-08-21 21:10:33 -0500

    changed the seeded comments, added a link to home on the logo, de-broed it

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import Auth from '../../utils/auth';
-import logo from '../../logo.png';
-import '../../index.css';
+import Auth from "../../utils/auth";
+import logo from "../../logo.png";
+import "../../index.css";
 
 const Header = () => {
   const logout = (event) => {
@@ -12,38 +12,48 @@ const Header = () => {
   };
   return (
     <header className="bg-primary text-light align-center">
-      
-        {/* Top nav bar buttons */}
-        <div className="container-fluid">
-          <div className="row text-right">
-              <div className="col-12 text-end navBar">
-              {Auth.loggedIn() ? (
-            <>
-              <span className="navLink rightBorder">Welcome {Auth.getProfile().data.username}!</span>
-              <button className="navLink rightBorder" onClick={logout}>
-                Logout
-              </button>
-            </>
-          ) : (
-            <>
-              <Link className="navLink rightBorder" to="/login">
-                Login
-              </Link>
-              <Link className="navLink rightBorder" to="/signup">
-                Signup
-              </Link>
-            </>
-          )}
-                    <Link to="/" className="navLink rightBorder">Home</Link>
-              </div>
-          </div>
-          {/* Logo */}
-          <div className="row">
-              <div className="col-12 text-center p-5">
-                <h1 className="mainTitle"><img className="homeLogo" src={logo}></img> TripBook</h1>
-              </div>
+      {/* Top nav bar buttons */}
+      <div className="container-fluid">
+        <div className="row text-right">
+          <div className="col-12 text-end navBar">
+            {Auth.loggedIn() ? (
+              <>
+                <span className="navLink rightBorder">
+                  Welcome {Auth.getProfile().data.username}!
+                </span>
+                <button className="navLink rightBorder" onClick={logout}>
+                  Logout
+                </button>
+              </>
+            ) : (
+              <>
+                <Link className="navLink rightBorder" to="/login">
+                  Login
+                </Link>
+                <Link className="navLink rightBorder" to="/signup">
+                  Signup
+                </Link>
+              </>
+            )}
+            <Link to="/" className="navLink rightBorder">
+              Home
+            </Link>
           </div>
         </div>
+        {/* Logo */}
+        <div className="row">
+          <div className="col-12 text-center p-5">
+            <Link to="/" className="mainTitle">
+              <img
+                className="homeLogo"
+                onclick="window.location.href= /pages/home.js'"
+                src={logo}
+              ></img>{" "}
+              TripBook
+            </Link>
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,7 +8,6 @@ import logo from "../logo.png";
 import "../index.css";
 import AllImages from "../components/ImageUpload/allImages";
 
-
 import { QUERY_TRIPS } from "../utils/queries";
 
 const Home = () => {
@@ -17,44 +16,38 @@ const Home = () => {
 
   return (
     <div className="row bg-light whiteCon pb-5">
-
-        <div className="col-12">
-            <div className="row btnRow">
-                {/* Home page main buttons */}
-                <div className="col-6 text-center p-5">
-                    <Link className="btn btn-primary" to="/trips">View Your Trips</Link>
-                </div>
-                <div className="col-6 p-5 text-center">
-                    <button className="btn btn-primary">Take a Trip!</button>
-                </div>
-                <div className="col-12">
-                    <div className="flex-row justify-center">
-                        <div
-                        className="col-12"
-                        style={{ border: '1px dotted #1a1a1a' }}
-                        >
-                        <TripForm />
-                        </div>
-                        <div className="">
-                        {loading ? (
-                            <div>Loading...</div>
-                        ) : (
-                            <TripList
-                            trips={trips}
-                            title="Let's Go Bro!"
-                            />
-                        )}
-                        </div>
-                    </div>
-                </div>
-
-                {/* Trip images */}
-
+      <div className="col-12">
+        <div className="row btnRow">
+          {/* Home page main buttons */}
+          <div className="col-6 text-center p-5">
+            <Link className="btn btn-primary" to="/trips">
+              View Your Trips
+            </Link>
+          </div>
+          <div className="col-6 p-5 text-center">
+            <button className="btn btn-primary">Take a Trip!</button>
+          </div>
+          <div className="col-12">
+            <div className="flex-row justify-center">
+              <div className="col-12" style={{ border: "1px dotted #1a1a1a" }}>
+                <TripForm />
+              </div>
+              <div className="">
+                {loading ? (
+                  <div>Loading...</div>
+                ) : (
+                  <TripList
+                    trips={trips}
+                    title="Are you ready for a vacation?"
+                  />
+                )}
+              </div>
             </div>
           </div>
 
-
-
+          {/* Trip images */}
+        </div>
+      </div>
     </div>
   );
 };
