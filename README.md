43f79de8c20b01d0dcac865339b0ef8971c4aa5a
S0519 <sarakanga666@gmail.com>
2021-08-24 17:00:00 +0530

    added hotel schema

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # TripBook
 
-
 Website which allows the user to find the best place to enjoy the vacation.
---
+++ server/models/Hotel.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const hotelSchema = new Schema({
+  hotelName: {
+    type: String,
+    required:true,
+    minlength: 1,
+    maxlength: 280,
+    trim: true,
+  },
+  hotelAddress: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  hotelPhone: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  hotelCost: {
+    type: Number,
+    required: true
+  },
+  hotelRating: {
+    type: Number,
+    required: true,
+    default: 1
+  },
+  hotelImages: [{imageName:{type: String}}],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+  }
+});
+
+const Hotel = model('Hotel', hotelSchema);
+
+module.exports = Hotel;
