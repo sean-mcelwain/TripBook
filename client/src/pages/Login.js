35b228a383bc4b03365bc58c17bba471f06105de
S0519 <sarakanga666@gmail.com>
2021-08-25 07:30:33 -0500

    remove prop parameter for functional component

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -4,7 +4,7 @@ import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
-const Login = (props) => {
+const Login = () => {
   const [formState, setFormState] = useState({ email: '', password: '' });
   const [login, { error, data }] = useMutation(LOGIN_USER);
 
